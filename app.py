fdf698a0ebde245ad05273835e03b9bbde2c30a3
Lalbuksh123 <160569177+Lalbuksh123@users.noreply.github.com>
2025-02-25 18:26:38 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.set_page_config(page_title= "growth mindset project", project_icon="★")
+st.set_page_config(page_title= "growth mindset project", page_icon="★")
 
 st.title("Growth Mindset Challange: Web App With Streamlit")
 
